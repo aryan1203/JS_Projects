50c3ef31e527cc87d1a160b587c34257d60f6cba
Aryan Singh <68284783+aryan1203@users.noreply.github.com>
2023-03-08 10:11:28 +0530

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,8 @@
+const toggleBtn=document.querySelector('.menu');
+const closeBtn=document.querySelector('.close');
+const sidebar=document.querySelector('.sidebar');
+
+toggleBtn.addEventListener("click",function(){
+    console.log(sidebar.classList);
+    // if(sidebar.classList.contains("sidebar"))
+});
